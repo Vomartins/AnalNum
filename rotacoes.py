bd8f0cb6ec4952ac2a834e1a1ed9dd99c9de89bd
PaSSoS97 <47029367+PaSSoS97@users.noreply.github.com>
2019-05-30 11:08:08 -0300

    Add files via upload

--- rotacoes.py
+++ rotacoes.py
@@ -1,63 +1,48 @@
 import numpy as np
 import sys
 
-def QRrot(A):
-    m = np.shape(A)[0]
-    n = np.shape(A)[1]
+class QR:
 
-    if(m<n):
-        sys.stderr.write("ERRO: a matriz não admite fatoração QR.")
-        input()
-        sys.exit()
+    def __init__(self,A):
+        self.__A = A
 
-    Q = np.identity(m, dtype=float) #Q^t
-    R = A.astype('float')
+    def rotacoes(self):
+        m = np.shape(self.__A)[0] #Número de linhas de A
+        n = np.shape(self.__A)[1] #Número de colunas de A
 
-    for i in range(n):
-        for j in range(i+1, m):
-            if(R[j,i] == 0): #A matriz de rotação é a identidade.
-                continue
-            
-            #Cálculo do cos() e sin()
-            N = max([abs(R[i,i]),abs(R[j,i])]) #Fator de Normalização
-            a = R[i,i]/N
-            b = R[j,i]/N
-            v = (a**2 + b**2)**0.5
+        if(m<n):
+            sys.stderr.write("ERRO: a matriz não admite fatoração QR.")
+            input()
+            sys.exit()
 
-            c = a/v
-            s = b/v
+        Q = np.identity(m, dtype=float) #Q^t
+        R = A.astype('float')
 
-            #Produto das matrizes
-            #Matriz R
-            Li = c*R[i] + s*R[j]
-            Lj = c*R[j] - s*R[i]
-            R[i] = Li.copy()
-            R[j] = Lj.copy()
+        for i in range(n):
+            for j in range(i+1, m):
+                if(R[j,i] == 0): #A matriz de rotação é a identidade.
+                    continue
+                
+                #Cálculo do cos() e sin()
+                N = max([abs(R[i,i]),abs(R[j,i])]) #Fator de Normalização
+                a = R[i,i]/N
+                b = R[j,i]/N
+                v = (a**2 + b**2)**0.5
 
-            #Matriz Q
-            Li = c*Q[i] + s*Q[j]
-            Lj = c*Q[j] - s*Q[i]
-            Q[i] = Li.copy()
-            Q[j] = Lj.copy()
-            
-    return Q.transpose(), R
+                c = a/v #cos
+                s = b/v #sin
 
-def HilbertMatrix(n):
-    A = np.zeros((n,n))
-    for i in range(n):
-        for j in range(n):
-            A[i,j] = 1/(i+j+1)
-    return A
+                #Produto das matrizes
+                #Matriz R
+                Li = c*R[i] + s*R[j]
+                Lj = c*R[j] - s*R[i]
+                R[i] = Li.copy()
+                R[j] = Lj.copy()
 
-def main():
-    A = HilbertMatrix(13)
-    Q,R = QRrot(A)
-
-    B = np.dot(Q,R)
-    B = A-B
-    print(np.linalg.eigvals(np.dot(B.transpose(),B)).max())
-   
-    input()
-
-main()
-
+                #Matriz Q
+                Li = c*Q[i] + s*Q[j]
+                Lj = c*Q[j] - s*Q[i]
+                Q[i] = Li.copy()
+                Q[j] = Lj.copy()
+                
+        return Q.transpose(), R
