11af76917c86fba0e3683992bb177ea7fa7bd37e
Vinícius <vm.omartins@outlook.com>
2019-06-16 12:27:24 -0300

    Auteração dos vetores retornados na LinSolve, ante: vetor coluna, agora: vetor linha. Construção da main para fazeras fatorações.

--- LinSolve.py
+++ LinSolve.py
@@ -6,9 +6,18 @@ class LinSolve:
         self.__A = A
         self.__b = b
         
-    #Identifica qual o tipo da matriz e chama a função correta    
+    #Identifica qual o tipo da matriz e chama a função correta
     def Solve(self):
-        
+        S = 0
+        for i in range(0, np.shape(self.__A)[0]):
+            for j in range(i+1, np.shape(self.__A)[0]):
+                S = S + self.__A[i, j]
+
+        if S <= 10**(-10):
+            return self.triinf()
+        else:
+            return self.trisup()
+        '''
         if self.__A[1,0] == 0 and self.__A[0,1] != 0:
             x1 = self.trisup()
             if (np.dot(self.__A, x1) - self.__b <= np.full(np.shape(self.__b),10**(-10))).all():
@@ -23,27 +32,27 @@ class LinSolve:
                 return "A matriz não é triangular."
         else:
             return "A matriz não é triangular."
-        
+        '''
     #Solução direta do sistem linear com matriz A triangular superior
     def trisup(self):
-        x = np.zeros((np.shape(self.__A)[0],1))
+        x = np.zeros((1,np.shape(self.__A)[0]))
         s = 0
         for j in range(np.shape(self.__A)[0]):
-            x[np.shape(self.__A)[0]-(j+1), 0] = (self.__b[np.shape(self.__A)[0]-(j+1)] - s)/ self.__A[np.shape(self.__A)[0]-(j+1),np.shape(self.__A)[0]-(j+1)]
+            x[0,np.shape(self.__A)[0]-(j+1)] = (self.__b[np.shape(self.__A)[0]-(j+1)] - s)/ self.__A[np.shape(self.__A)[0]-(j+1),np.shape(self.__A)[0]-(j+1)]
             s = 0
             if j < np.shape(self.__A)[0]-1:
                 for i in range(j+1):
-                    s = s + x[np.shape(self.__A)[0]-(i+1), 0]*self.__A[np.shape(self.__A)[0]-(j+2), np.shape(self.__A)[0]-(i+1)]
+                    s = s + x[0,np.shape(self.__A)[0]-(i+1)]*self.__A[np.shape(self.__A)[0]-(j+2), np.shape(self.__A)[0]-(i+1)]
         return x
 
     #Solução direta do sistem linear com matriz A triangular inferior
     def triinf(self):
-        x = np.zeros((np.shape(self.__A)[0],1))
+        x = np.zeros((1,np.shape(self.__A)[0]))
         s = 0
         for j in range(np.shape(self.__A)[0]):
-            x[j, 0] = (self.__b[j] - s)/ self.__A[j,j]
+            x[0,j] = (self.__b[j] - s)/ self.__A[j,j]
             s = 0
             if j < np.shape(self.__A)[0]-1:
                 for i in range(j+1):
-                    s = s + x[i, 0]*self.__A[j+1, i]
+                    s = s + x[0,i]*self.__A[j+1, i]
         return x
--- main.py
+++ main.py
@@ -4,61 +4,94 @@ from Vetor import Vetor, Dot
 from Matriz import Matriz
 from Fatoracao import Fat
 
-n = 3
-
+print('\n')
+print('*************** Análise  numérica ***************')
+n = int(input('Qual a dimensão da matriz de Hilbert desejada? '))
+print('Códigos dos Algortimos:')
+print('    (1) Cholesky \n    (2) QR com Gram-Schmidt \n    (3) QR com rotação')
+alg = int(input('Qual o algoritmo desejado? '))
+      
 #Matriz de Hilbert
 H = np.fromfunction(lambda i, j: 1/(i+j+1), (n,n), dtype=int)
+I = np.identity(n)
 
 #Inicialização do objeto Fat(), a partir dele podemos calcular as três fatorações da matriz de Hilbert.
 A = Fat(H)
+      
+#Cálculo das fatorações de acordo com o algoritmo escolhido a partir do objeto A e inicialização dos objetos Matriz com as fatorações obtidas.
+if alg == 1:
+    R = Matriz(A.cholesky()) #Cholesky
+    #Resolução do sistema HX=I com cholesky.
+    Y = np.zeros((1, n))
+    for i in range(n):
+        y = LinSolve(R.matriz.transpose(), I[:, i]).Solve()
+        Y = np.vstack((Y, y))
+    Y = np.delete(Y, 0, 0)
 
-#Cálculo das fatorações a partir do objeto A e inicialização dos objetos Matriz com as fatorações obtidas.
-R = Matriz(A.cholesky()) #Cholesky
-q1, r1 = A.GS() #QR com Gram-Schmidt
-Q1 = Matriz(q1)
-R1 = Matriz(r1)
-q2, r2 = A.rot() #QR rotação
-Q2 = Matriz(q2)
-R2 = Matriz(r2)
+    X = np.zeros((1, n))
+    for i in range(n):
+        x = LinSolve(R.matriz, Y[i, :]).Solve()
+        X = np.vstack((X, x))
+    X = np.delete(X, 0, 0)
+    In = np.dot(H, X)
+    
+    if n <= 3:
+        print('A fatoração de Cholesky é a seguinte:')
+        print('Matriz R:')
+        print(R.matriz)
+        print('Matriz Rt:')
+        print(R.transpose)
 
-#Resolução do sistema HX=I com cholesky.
-y1 = LinSolve(R.matriz.transpose(), I[:, 0]).triinf()
-y2 = LinSolve(R.matriz.transpose(), I[:, 1]).triinf()
-y3 = LinSolve(R.matriz.transpose(), I[:, 2]).triinf()
+      
+    print('Com a fatoração de Cholesky, a inversa da matriz de Hibert de ordem {}'.format(n))
+    print(X)
+    print('H*X:')
+    print(In)
+elif alg == 2:
+    q1, r1 = A.GS() #QR com Gram-Schmidt
+    Q1 = Matriz(q1)
+    R1 = Matriz(r1)
+    #Resolução do sistema HX=I com Gram-Schmidt.
+    X = np.zeros((1,n))
+    for i in range(n):
+        x = LinSolve(R1.matriz, Q1.transpose[:, i]).Solve()
+        X = np.vstack((X,x))
+    X = np.delete(X, 0, 0)
+    In = np.dot(H, X)
+    
+    if n <= 3:
+        print('A fatoração de QR com Gram-Schmidt é a seguinte:')
+        print('Matriz Q:')
+        print(Q1.matriz)
+        print('Matriz R:')
+        print(R1.matriz)
+      
+    print('Com a fatoração de QR com Gram-Schmidt, a inversa da matriz de Hibert de ordem {}'.format(n))
+    print(X)
+    print('H*X:')
+    print(In)
+elif alg == 3:
+    q2, r2 = A.rot() #QR rotação
+    Q2 = Matriz(q2)
+    R2 = Matriz(r2)
+    #Resolução do sistema HX=I com rotações.
+    X = np.zeros((1,n))
+    for i in range(n):
+        x = LinSolve(R2.matriz, Q2.transpose[:, i]).Solve()
+        X = np.vstack((X,x))
+    X = np.delete(X, 0, 0)
+    In = np.dot(H, X)
+    
+    if n <= 3:
+        print('A fatoração de QR com rotações é a seguinte:')
+        print('Matriz Q:')
+        print(Q2.matriz)
+        print('Matriz R:')
+        print(R2.matriz)
 
-Y = np.array([y1, y2, y3])
-
-x1 = LinSolve(R.matriz, Y[0, :]).trisup()
-x2 = LinSolve(R.matriz, Y[1, :]).trisup()
-x3 = LinSolve(R.matriz, Y[2, :]).trisup()
-
-X = np.array([x1, x2, x3]).transpose()
-If = np.dot(H, X)
-
-print(X)
-print(If)
-
-#Resolução do sistema HX=I com Gram-Schmidt.
-x1 = LinSolve(R1.matriz, Q1.transpose[:, 0]).trisup()
-x2 = LinSolve(R1.matriz, Q1.transpose[:, 1]).trisup()
-x3 = LinSolve(R1.matriz, Q1.transpose[:, 2]).trisup()
-
-X = np.array([x1, x2, x3]).transpose()
-If = np.dot(H, X)
-
-print(X)
-print(If)
-
-#Resolução do sistema HX=I com rotações.
-x1 = LinSolve(R2.matriz, Q2.transpose[:, 0]).trisup()
-x2 = LinSolve(R2.matriz, Q2.transpose[:, 1]).trisup()
-x3 = LinSolve(R2.matriz, Q2.transpose[:, 2]).trisup()
-
-X = np.array([x1, x2, x3]).transpose()
-If = np.dot(H, X)
-
-print(X)
-print(If)
-
-
-input()
+    print('Com a fatoração de QR com rotações, a inversa da matriz de Hibert de ordem {}'.format(n))
+    print(X)
+    print('H*X:')
+    print(In)
+else:
+      print('Código inválido.')
